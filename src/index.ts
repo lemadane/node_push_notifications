a471dac848b4e01fd29e5ec73d8a4f63402fdc8a
lem <lem@email.com>
2019-03-17 12:51:37 +0800

    refactor(.gitignore): add yarn.lock and yarn-error.log to .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { error, log } from "console";
-import * as express from "express";
-import { join } from "path";
-import * as webpush from "web-push";
+import { error, log } from 'console';
+import * as express from 'express';
+import { join } from 'path';
+import * as webpush from 'web-push';
 
 const app = express()
 
